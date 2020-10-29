0bd1a26e7739ac9752624a351d7b116aa97a67b3
ReneRugaba <rugabarj@gmail.com>
2020-10-29 18:46:22 +0100

    finalisation class4

--- connexionbdd/class4/Manager.php
+++ connexionbdd/class4/Manager.php
@@ -2,27 +2,28 @@
 
 include_once('Personne1.php');
 
-class Manager extends Personne{
+class Manager extends Personne
+{
     private $service;
 
-    public function __construct(int $id,string $nom,string $prenom,string $mail,string $telephone,float $salaire,$service)
+    public function __construct(int $id, string $nom, string $prenom, string $mail, string $telephone, float $salaire, $service)
     {
-        $this->id=$id;
-        $this->nom=$nom;
-        $this->prenom=$prenom;
-        $this->telephone=$telephone;
-        $this->salaire=$salaire;
-        $this->mail=$mail;
-        self::$counter=self::$counter+1;
-        $this->service=$service;
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->telephone = $telephone;
+        $this->salaire = $salaire;
+        $this->mail = $mail;
+        self::$counter = self::$counter + 1;
+        $this->service = $service;
     }
 
-    
+
 
     /**
      * Get the value of service
-     */ 
-    public function getService():string
+     */
+    public function getService(): string
     {
         return $this->service;
     }
@@ -31,20 +32,21 @@ class Manager extends Personne{
      * Set the value of service
      *
      * @return  self
-     */ 
-    public function setService(string $service):self
+     */
+    public function setService(string $service): self
     {
         $this->service = $service;
 
         return $this;
     }
 
-    public function calculSalaire():float
+    public function calculSalaire(): float
     {
-        return $this->salaire *1.35;
+        return $this->salaire * 1.35;
     }
 
-    public function affiche():string {
+    public function affiche(): string
+    {
         return $this->service;
     }
 }
--- connexionbdd/class4/teste1.php
+++ connexionbdd/class4/teste1.php
@@ -1,12 +1,19 @@
 <?php
 include_once('Developpeur.php');
+include_once('Manager.php');
 
-$dev= new Developpeur(1,'martin','marcus','m@mail.com','062151321',13000,'backend');
-echo $dev. "\n";
-$dev2= new Developpeur(2,'MRS','madame','f@mail.com','062551321',15000,'backend');
-echo $dev2. "\n";
+$dev = new Developpeur(1, 'martin', 'marcus', 'm@mail.com', '062151321', 13000, 'backend');
+echo $dev . "\n";
+$dev2 = new Developpeur(2, 'MRS', 'madame', 'f@mail.com', '062551321', 15000, 'backend');
+echo $dev2 . "\n";
 
-echo $dev->calculSalaire(). "\n";
-echo $dev2->calculSalaire();
+echo "Le salaire du développeur " . $dev->getNom() . " est: " . $dev->calculSalaire() . " , sa spécialité :" . $dev->getSpecialite() . "\n";
+echo "Le salaire du développeur " . $dev2->getNom() . " est: " . $dev2->calculSalaire() . " , sa spécialité :" . $dev2->getSpecialite() . "\n";
 
+$man = new Manager(1, 'martin', 'marcus', 'm@mail.com', '062151321', 13000, 'backend');
+echo $dev . "\n";
+$man2 = new Manager(2, 'MRS', 'madame', 'f@mail.com', '062551321', 15000, 'backend');
+echo $dev2 . "\n";
 
+echo "Le salaire du Manager " . $man->getNom() . " est: " . $man->calculSalaire() . " , sa spécialité :" . $man->getService() . "\n";
+echo "Le salaire du Manager " . $man2->getNom() . " est: " . $man2->calculSalaire() . " , sa spécialité :" . $man2->getService();

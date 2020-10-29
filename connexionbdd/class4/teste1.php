34219598d00a5a1fb37584cb74b9ced15ae18a67
rugaba_rene <rugabarj@gmail.com>
2020-10-29 16:27:25 +0100

    continu

--- connexionbdd/class4/teste1.php
+++ connexionbdd/class4/teste1.php
@@ -4,8 +4,9 @@ include_once('Developpeur.php');
 $dev= new Developpeur(1,'martin','marcus','m@mail.com','062151321',13000,'backend');
 echo $dev. "\n";
 $dev2= new Developpeur(2,'MRS','madame','f@mail.com','062551321',15000,'backend');
+echo $dev2. "\n";
 
-
-echo $dev2;
+echo $dev->calculSalaire(). "\n";
+echo $dev2->calculSalaire();
 
 
